6177e293d5d825360b1b24e6fe5998bc393d37fa
s.anisin <s.anisin>
2022-10-17 13:50:50 +0600

    Внедрение зависимостей: repository, services

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/StepanAnisin/gin-rest-api"
 	"github.com/StepanAnisin/gin-rest-api/pkg/handler"
+	"github.com/StepanAnisin/gin-rest-api/pkg/repository"
+	"github.com/StepanAnisin/gin-rest-api/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
