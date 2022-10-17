28d0b7d6954c4ea66fd2a570128dc750ac820a6b
s.anisin <s.anisin>
2022-10-17 14:44:28 +0600

    создал конфиг, импорт конфигов с помощью библиотеки viper

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"github.com/StepanAnisin/gin-rest-api/pkg/handler"
 	"github.com/StepanAnisin/gin-rest-api/pkg/repository"
 	"github.com/StepanAnisin/gin-rest-api/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occured while initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running htt server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
