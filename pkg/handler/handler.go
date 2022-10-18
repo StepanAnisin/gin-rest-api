83eef66fd1415a1628b5e898f9e4d00b0239c346
s.anisin <s.anisin>
2022-10-19 02:17:08 +0600

    Добавил комментрии

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -20,6 +20,9 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	{
 		auth.POST("/sign-up", h.signUp)
 		auth.POST("/sign-in", h.signIn)
+
+		//Вызов метода userIdentity для дальнейшей реализации api
+		//api := router.Group("/api", h.userIdentity)
 	}
 	return router
 }
