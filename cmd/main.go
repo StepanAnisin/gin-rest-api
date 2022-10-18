aeada10c8375c9fd35a32fc3219099914f2a4a55
s.anisin <s.anisin>
2022-10-18 18:12:25 +0600

    Добавил конфиг для подключения к базе данных

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-	"github.com/StepanAnisin/gin-rest-api"
+	"log"
+
+	todo "github.com/StepanAnisin/gin-rest-api"
 	"github.com/StepanAnisin/gin-rest-api/pkg/handler"
 	"github.com/StepanAnisin/gin-rest-api/pkg/repository"
 	"github.com/StepanAnisin/gin-rest-api/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
-	"log"
 )
 
 func main() {
+
 	if err := initConfig(); err != nil {
 		log.Fatalf("error occured while initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.dbpassword"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize database %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
---
+++ pkg/repository/postgres.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+// postgres://{user}:{password}@{hostname}:{port}/{database-name}?sslmode=disable
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	conString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
+	db, err := sqlx.Connect("postgres", conString)
+	if err != nil {
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
